157c24fc70b865054ae26dec16a0cf28c378bbf4
oscar9181 <oscarokola17@gmail.com>
2022-05-08 17:16:18 +0300

    add html templates

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,44 @@
-from flask import Flask,render_template
-from app import app
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+pitches = [
+   {
+    'author' :'Sophie Paxton',
+    'title'  :'Interface Design',
+    'pitch'  :'Animation is like cursing. If you overuse it, it loses all its impact.',
+    'date_posted' : 'October 17 2002'
+   },
+   {
+    'author' :'Jessica Cherner',
+    'title'  :'Architectural Design.',
+    'pitch'  :'On the famed Billionaire Row, SHoP Architects and Studio Sofield have delivered on a long-awaited promise',
+    'date_posted' :'April 11 2022'
+   },
+   {      
+    'author' :'Jeff Bezos',
+    'title'  :'Forbes',
+    'pitch'  :'I have not failed. I have just found 10,000 ways that wil not work.',
+    'date_posted' :'May 11 2008'
+   },
+   {
+    'author' :'Oscar',
+    'title'  :'Advertisement',
+    'pitch'  :'A walk in the moon book ',
+    'date_posted' :'March 13 2010'
+     }
+    ]
+
 
 @app.route('/')
 @app.route('/home')
-def home():
-
+def home():    
     '''
     View root page function that returns the home page and its data
     '''
+    return render_template('home.html',pitches=pitches)
 
-    
-    return render_template('home.html')
+
+if __name__ == '__main__':
+       app.run()
