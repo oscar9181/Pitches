e8cb165b46fcf806e17ca27a10be28e2361238bc
oscar9181 <oscarokola17@gmail.com>
2022-05-09 18:28:10 +0300

    install sqlalchemy

--- routes.py
+++ routes.py
@@ -1,10 +1,24 @@
-from crypt import methods
+from email.policy import default
 from flask import Flask, render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, column
+
 from forms import  RegistrationForm,LoginForm
 
 app =Flask(__name__)
-
 app.config['SECRET_KEY'] = 'ac4d969b60'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///:site.db'
+db = SQLAlchemy(app)
+
+class User(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20), unique=True,nullable=False)
+    email = db.Column(db.String(100), unique=True,nullable=False)
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
+    password =  db.Column(db.String(70),nullable=False)
+
+def __repr__(self):
+    return f"User('{self.username}','{self.email}','{self.image_file}')"
 
 pitches = [
    {
